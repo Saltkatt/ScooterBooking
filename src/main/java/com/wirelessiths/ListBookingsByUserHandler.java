a185eb622adadd11721405817f17d7f169477eff
adam <skiryoland@gmail.com>
2019-08-21 11:32:27 +0200

    add logging config and apache log dependencies

---
+++ src/main/java/com/wirelessiths/ListBookingsByUserHandler.java
@@ -0,0 +1,73 @@
+package com.wirelessiths;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.wirelessiths.dal.Booking;
+import com.wirelessiths.exception.UnableToListBookingsException;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ListBookingsByUserHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        try {
+
+            logger.error(context.toString());
+            // get all users
+            List<Booking> bookings = new Booking().list();
+
+            // send the response back
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(bookings)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+
+        } catch(UnableToListBookingsException ex){
+            logger.error("Error in listing bookings: " + ex);
+            logger.error(ex.getMessage());
+            ex.printStackTrace();
+
+            // send the error response back
+            Response responseBody = new Response("Error in listing bookings due to state: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+
+        }catch (IOException ex){
+            logger.error("Error in listing bookings due to I/O: " + ex);
+            logger.error(ex.getMessage());
+            ex.printStackTrace();
+
+            // send the error response back
+            Response responseBody = new Response("Error in I/O when listing bookings: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+
+        } catch(Exception ex) {
+            logger.error("Error in listing users: " + ex);
+            logger.error(ex.getMessage());
+            ex.printStackTrace();
+
+            // send the error response back
+            Response responseBody = new Response("Error in listing bookings: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
+}
