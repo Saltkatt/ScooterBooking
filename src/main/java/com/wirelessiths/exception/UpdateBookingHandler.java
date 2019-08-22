8afe68f9a19261153252c78c3eeb2f3e6dbda8d1
robin hansson <robin-hansson@hotmail.com>
2019-08-22 15:32:09 +0200

    Added update functionality

---
+++ src/main/java/com/wirelessiths/exception/UpdateBookingHandler.java
@@ -0,0 +1,89 @@
+package com.wirelessiths.exception;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wirelessiths.ApiGatewayResponse;
+import com.wirelessiths.Response;
+import com.wirelessiths.dal.Booking;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+public class UpdateBookingHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+        try {
+
+            // get the 'body' from input
+            JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
+
+            // get the 'pathParameters' from input
+            Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+            String productId = pathParameters.get("id");
+
+            // get the Product by id
+            Booking booking = new Booking().get(productId);
+
+            // send the response back
+            if (booking != null) {
+
+                try {
+                    booking.setBookingId(String.valueOf(Optional.ofNullable(body.get("username").asText())));
+                    booking.setScooterId(String.valueOf(Optional.ofNullable(body.get("firstName").asText())));
+                    booking.setUserId(String.valueOf(Optional.ofNullable(body.get("lastName").asText())));
+                    booking.setStartTime(LocalDateTime.parse(String.valueOf(Optional.ofNullable(body.get("email").asText()))));
+                    booking.setEndTime(LocalDateTime.parse(String.valueOf(Optional.ofNullable(body.get("password").asText()))));
+                    booking.update(booking);
+
+                } catch (Exception e) {
+
+                    logger.error("Error in retrieving product: " + e);
+
+                    // send the error response back
+                    Response responseBody = new Response("Error in updating product: ", input);
+                    return ApiGatewayResponse.builder()
+                            .setStatusCode(500)
+                            .setObjectBody(responseBody)
+                            .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                            .build();
+                }
+
+                return ApiGatewayResponse.builder()
+                        .setStatusCode(200)
+                        .setObjectBody(booking)
+                        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                        .build();
+            }
+            else {
+
+                return ApiGatewayResponse.builder()
+                        .setStatusCode(404)
+                        .setObjectBody("Product with id: '" + productId + "' not found.")
+                        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                        .build();
+            }
+
+        }catch (Exception ex) {
+
+            logger.error("Error in retrieving product: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in retrieving product: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
+}
