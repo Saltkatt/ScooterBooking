4c36db39cac5bba84ae1fcb70f86fb796378316b
AdamBremholm <skiryoland@gmail.com>
2019-08-28 16:51:57 +0200

    Add working cognito login.

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <h3>Welcome, </h3>
+      <div class="alert alert-info">
+        {{userInfo}}
+      </div>
+      <router-link to="/logout">
+        Logout
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+  import UserInfoStore from '../app/user-info-store';
+  export default {
+    name: 'Home',
+    data: function() {
+      return{
+        userInfo: UserInfoStore.state.cognitoInfo
+      }
+    }
+  }
+</script>
+<style>
+</style>
