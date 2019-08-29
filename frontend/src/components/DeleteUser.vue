eda09ba9254c454789a2cb9d3b13c094f8e34b16
Elin Sexton <elin.sexton@gmail.com>
2019-08-29 14:04:54 +0200

    Created DeleteUser component

---
+++ frontend/src/components/DeleteUser.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="delete-user-content">
+        <div>
+            <h1>Delete Account</h1>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DeleteUser"
+    }
+</script>
+
+<style scoped>
+
+</style>
