8e430a0434a509417bc9f04928ccfbef0bee3061
Elin Sexton <elin.sexton@gmail.com>
2019-08-30 16:36:15 +0200

    Added scooter.png, made CSS changes to Home and App.

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="row">
     <div class="col">
-      <h3>Welcome, </h3>
-      <div class="alert alert-info">
-        {{userInfo}}
-      </div>
-      <router-link to="/logout">
-        Logout
-      </router-link>
+
+      <header>
+        <!-- Navigation Bar -->
+        <nav class="nav-bar">
+          <ul>
+            <li><router-link to="/logout"><button class="btn">Logout</button></router-link></li>
+            <!--<li><router-link to="/delete"><button class="btn">Delete</button></router-link></li>-->
+              <li>Stats</li>
+              <li>Trips</li>
+
+
+          </ul>
+        </nav>
+
+        <h1>Welcome {{userInfo.username}}</h1>
+        <div class="alert alert-info">
+          {{userInfo.email}}
+        </div>
+
+        </header>
+
+        <div class="current-bookings">
+            <h2>Current Bookings</h2>
+        </div>
+
+        <div class="expired-bookings">
+            <h2>Expired Bookings</h2>
+        </div>
+
+
+
     </div>
   </div>
 </template>
@@ -23,4 +47,29 @@
   }
 </script>
 <style>
+  .nav-bar{
+    background-color: azure;
+
+  }
+  ul{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  li{
+    display: inline;
+    margin: 5%;
+  }
+  .btn{
+    cursor: pointer;
+      margin-right: 5px;
+      float: right;
+  }
+
+  .alert{
+      color: black;
+  }
+
+
 </style>
