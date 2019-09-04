960eb297dd209f72c8fb103f2aa467c896df1bd7
Carl Ljungberg <ljungberg125@gmail.com>
2019-09-04 15:27:51 +0200

    add checkoutScooterHandler

--- src/test/java/com/wirelessiths/test/Main.java
+++ src/test/java/com/wirelessiths/test/Main.java
@@ -1,7 +1,15 @@
 package com.wirelessiths.test;
 
+import java.time.Instant;
+
 public class Main {
 
     public static void main(String[] args) {
+        Instant start = Instant.now().minusSeconds(60 * 10);
+        Instant now = Instant.now();
+
+        System.out.println("start: " + start);
+        System.out.println("now: " + now );
+        System.out.println(now.isAfter(start) && now.isBefore(start.plusSeconds(60 * 20)));
     }
 }
