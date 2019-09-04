48e6b38f3b68e01d6051a93ea2fd5178ca7e4128
Carl Ljungberg <ljungberg125@gmail.com>
2019-09-04 18:35:45 +0200

    add handler for activating booking

--- src/main/java/com/wirelessiths/dal/Booking.java
+++ src/main/java/com/wirelessiths/dal/Booking.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.*;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
+import com.wirelessiths.exception.CouldNotCreateBookingException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
--- src/test/java/com/wirelessiths/test/Main.java
+++ src/test/java/com/wirelessiths/test/Main.java
@@ -1,15 +1,46 @@
 package com.wirelessiths.test;
 
-import java.time.Instant;
+import com.wirelessiths.dal.Booking;
+import com.wirelessiths.dal.TripStatus;
+
+import java.time.*;
+import java.util.TimeZone;
 
 public class Main {
 
     public static void main(String[] args) {
-        Instant start = Instant.now().minusSeconds(60 * 10);
-        Instant now = Instant.now();
 
-        System.out.println("start: " + start);
+//        ZoneId stockholm = ZoneId.of("Europe/Stockholm");
+//
+//        Instant start = Instant.now().minusSeconds(60 * 10);
+//        //Instant now = Instant.now(Clock.system(ZoneId.of("Asia/Seoul")));
+//        Instant test = Instant.now(Clock.system(ZoneId.of("Europe/Stockholm")));
+//        Instant test2 = Instant.now();
+
+
+        LocalDateTime now = LocalDateTime.now();
+        //Instant now2 = Instant.from(now);
+        //Instant instant = now.atZone(ZoneId.of("Europe/Stockholm")).toInstant();
+        //Instant instant1 = now.toInstant(ZoneOffset.ofHours(0));
+        //ZoneOffset offset = ZoneOffset.UTC;
+        Instant instant1 = now.toInstant(ZoneOffset.UTC);
+
+        //ZoneId stockholm = ZoneId.of("Europe/Stockholm");
+        //ZonedDateTime time = ZonedDateTime.ofInstant(now, stockholm);
+
+        //System.out.println("start: " + start);
         System.out.println("now: " + now );
-        System.out.println(now.isAfter(start) && now.isBefore(start.plusSeconds(60 * 20)));
+        //System.out.println("time: " + time);
+        //System.out.println("test: " + test);
+
+        System.out.println("localdatetime: " + now);
+        System.out.println("instant1: " + instant1);
+        //System.out.println(now.isAfter(start) && now.isBefore(start.plusSeconds(60 * 20)));
+
+        System.out.println(TripStatus.WAITING_TO_START);
+        Booking b = new Booking();
+        b.setTripStatus(TripStatus.WAITING_TO_START);
+        System.out.println(b.getTripStatus());
+        System.out.println(b.getTripStatus().equals(TripStatus.WAITING_TO_START));
     }
 }
