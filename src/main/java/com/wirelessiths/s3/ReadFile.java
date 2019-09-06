e0268682cf5aa5a45adcfba00fbee15bc62ec1d9
Elin Sexton <elin.sexton@gmail.com>
2019-09-06 10:02:22 +0200

    Added code to extract values from file in S3 bucket using hashmap.

--- src/main/java/com/wirelessiths/s3/ReadFile.java
+++ src/main/java/com/wirelessiths/s3/ReadFile.java
@@ -10,12 +10,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class ReadFile {
 
 
-   public static void readFileInBucket() {
+   public static Map<String, Integer> readFileInBucket() {
        String keyName = "admin.txt";
        String bucketName = "booking-admin-settings1567688833719";
 
@@ -24,23 +25,29 @@ public class ReadFile {
        InputStream objectData = object.getObjectContent();
        //process the objectData stream
 
+       HashMap<String, Integer> setMap = new HashMap<>();
        try {
            BufferedReader reader = new BufferedReader(new InputStreamReader(object.getObjectContent()));
            String line;
-           HashMap<String, String> setMap = new HashMap<>();
-
-           //parse String to int
 
            while ((line = reader.readLine()) != null) {
-               String[]settings= line.split("=");
-               setMap.put(settings[0], settings[1]);
-               System.out.println(setMap);
+               if(line.contains("=")){
+                   String[]settings= line.split("=");
+                   setMap.put(settings[0], Integer.parseInt(settings[1]));
+               }
            }
+
+           //System.out.println(setMap);
+
+         /*  for (Map.Entry<String, Integer> entry : setMap.entrySet()) {
+               System.out.println( entry.getKey() + " "+ entry.getValue());
+           }*/
 
            objectData.close();
        } catch (IOException e) {
            e.printStackTrace();
        }
+       return setMap;
    }
 
 }
