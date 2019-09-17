32a0e487d222e9b132c750832fd7bb2d2f3f87fd
Carl Ljungberg <ljungberg125@gmail.com>
2019-09-17 11:48:50 +0200

    clean up stuff

--- src/main/java/com/wirelessiths/monitor/MonitorEndedBookingsFake.java
+++ src/main/java/com/wirelessiths/monitor/MonitorEndedBookingsFake.java
@@ -1,6 +1,5 @@
 package com.wirelessiths.monitor;
 
-import com.amazonaws.secretsmanager.caching.SecretCache;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import com.amazonaws.services.secretsmanager.model.*;
@@ -28,7 +27,6 @@ public class MonitorEndedBookingsFake {
     private Dotenv dotenv = Dotenv.load();
     private String baseUrl = dotenv.get("BASE_URL");
     private String tripEndpoint = dotenv.get("TRIP_ENDPOINT");
-    private String vehicleId = dotenv.get("SCOOTER_ID");
     private String authHeader = dotenv.get("AUTH");
     private ObjectMapper objectMapper = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
@@ -50,19 +48,12 @@ public class MonitorEndedBookingsFake {
 
 
     public void doStuff(){
-        //cashing secret
-        //private final SecretCache cache = new SecretCache();
-        //final String secret = cache.getSecretString("");
 
-        //check if ended not cancelled bookings
+        //check if ended uncancelled bookings
         //if not log and return
         //else log and check p&j for trips
         //if no trips log and return
         //else log and append trip to booking
-
-        //how query for dates??
-
-        //String secret = getSecret();
 
         Booking booking = new Booking();
         List<Booking> endedBookings = null;
@@ -79,7 +70,6 @@ public class MonitorEndedBookingsFake {
         }
         logger.info("number of bookings ended: " + endedBookings.size());
 
-        //String url = String.format("%s/%s%s", baseUrl, vehicleId, tripEndpoint);
         endedBookings.forEach((endedBooking)->{
 
             String url = String.format("%s/%s%s", baseUrl, endedBooking.getScooterId(), tripEndpoint);
@@ -95,13 +85,13 @@ public class MonitorEndedBookingsFake {
                 }else{
                     List<Trip> newTrips = objectMapper.convertValue(trips, new TypeReference<List<Trip>>(){});
                     newTrips.forEach(trip->{
-                        if(trip.getStartTime().isAfter(endedBooking.getStartTime()) && trip.getEndTime().isBefore(endedBooking.getEndTime())){
+
+                        if(trip.getStartTime().isAfter(endedBooking.getStartTime()) &&
+                           trip.getEndTime().isBefore(endedBooking.getEndTime())){
                             logger.info("appending trip to booking: " + endedBooking);
                             endedBooking.getTrips().add(trip);
                         }
                     });
-                    //logger.info("appending " + trips.size() + " new trips to booking: " + endedBooking);
-                    //endedBooking.getTrips().addAll(newTrips);
                 }
 
             }catch(IOException e){
@@ -109,65 +99,6 @@ public class MonitorEndedBookingsFake {
             }catch(Exception e){
                 logger.info("something went wrong: " + e.getMessage());
             }
-
         });
-        //logger.info(secret);
-    }
-
-    public static String getSecret() {
-
-        String secretName = "test-secret";
-        String region = "us-east-1";
-
-        // Create a Secrets Manager client
-        AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
-                .withRegion(region)
-                .build();
-
-        // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-        // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        // We rethrow the exception by default.
-
-        String secret, decodedBinarySecret;
-        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
-                .withSecretId(secretName);
-        GetSecretValueResult getSecretValueResult = null;
-
-        try {
-            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
-        } catch (DecryptionFailureException e) {
-            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (InternalServiceErrorException e) {
-            // An error occurred on the server side.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (InvalidParameterException e) {
-            // You provided an invalid value for a parameter.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (InvalidRequestException e) {
-            // You provided a parameter value that is not valid for the current state of the resource.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        } catch (ResourceNotFoundException e) {
-            // We can't find the resource that you asked for.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw e;
-        }
-
-        // Decrypts secret using the associated KMS CMK.
-        // Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if (getSecretValueResult.getSecretString() != null) {
-            secret = getSecretValueResult.getSecretString();
-            return secret;
-
-        }
-        else {
-            decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
-            return decodedBinarySecret;
-        }
-        // Your code goes here.
     }
 }
