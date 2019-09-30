f46018e63984ca89db0194735a2275671992a79a
Carl Ljungberg <ljungberg125@gmail.com>
2019-10-01 00:36:29 +0200

    after rebase

--- src/test/java/com/wirelessiths/dal/BookingQueryTest.java
+++ src/test/java/com/wirelessiths/dal/BookingQueryTest.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
